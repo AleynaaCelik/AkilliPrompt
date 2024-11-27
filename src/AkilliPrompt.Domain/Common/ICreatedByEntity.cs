1a24449e15ebc08472b1315a6e74e67aea405542
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-27 23:53:06 +0300

    ICreatedByEntity Created

---
+++ src/AkilliPrompt.Domain/Common/ICreatedByEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Common
+{
+    public interface ICreatedByEntity
+    {
+        string? CreatedByUserId { get; set; }
+        DateTimeOffset CreatedAt { get; set; }
+    }
+}
