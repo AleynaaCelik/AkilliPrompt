4f7495562420fe7531477a1fd6cb8f7439b84429
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-27 23:51:30 +0300

    EntityBase Created

---
+++ src/AkilliPrompt.Domain/Common/EntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Common
+{
+    public abstract   class EntityBase
+    {
+        public long Id { get; set; }
+        public string? CreatedByUserId { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public string? ModifiedByUserId { get; set; }
+        public DateTimeOffset? ModifiedAt { get; set; }
+    }
+}
