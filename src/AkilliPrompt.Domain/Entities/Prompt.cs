bd1bce02f5833eb2057e7848bba85ab5d794115c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-28 12:33:54 +0300

    Entities Created

---
+++ src/AkilliPrompt.Domain/Entities/Prompt.cs
@@ -0,0 +1,18 @@
+﻿using AkilliPrompt.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Entities
+{
+    public sealed  class Prompt:EntityBase
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+
+        public ICollection<PromptCategory> PromptCategories { get; set; } = [];
+    }
+}
