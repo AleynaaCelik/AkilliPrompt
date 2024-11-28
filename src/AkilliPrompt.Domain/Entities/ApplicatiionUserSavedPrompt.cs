2631c60fd8df71cc06e102c5aeb5bff661c3fe1d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-28 15:37:31 +0300

    ApplicatiionUserSavedPrompt Created

---
+++ src/AkilliPrompt.Domain/Entities/ApplicatiionUserSavedPrompt.cs
@@ -0,0 +1,20 @@
+﻿using AkilliPrompt.Domain.Common;
+using AkilliPrompt.Domain.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Entities
+{
+    public sealed  class ApplicatiionUserSavedPrompt:EntityBase
+    {
+        public long ApplicationUserId {  get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public long PromptId {  get; set; }
+        public  Prompt Prompt { get; set; }
+
+    }
+}
