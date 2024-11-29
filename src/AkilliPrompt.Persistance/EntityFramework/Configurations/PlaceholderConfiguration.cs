4c9a08976b1d04be67f102328b38cc5d1585d916
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-29 16:51:22 +0300

    PlaceholderConfiguration Created

---
+++ src/AkilliPrompt.Persistance/EntityFramework/Configurations/PlaceholderConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using AkilliPrompt.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Persistance.EntityFramework.Configurations
+{
+    public sealed class PlaceholderConfiguration : IEntityTypeConfiguration<PlaceHolder>
+    {
+        public void Configure(EntityTypeBuilder<PlaceHolder> builder)
+        {
+            // Id
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+
+            // Name
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            // CreatedAt
+            builder.Property(p => p.CreatedAt)
+                .IsRequired();
+
+            // CreatedByUserId
+            builder.Property(p => p.CreatedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(100);
+
+            // ModifiedAt
+            builder.Property(p => p.ModifiedAt)
+                .IsRequired(false);
+
+            // ModifiedByUserId
+            builder.Property(p => p.ModifiedByUserId)
+                .IsRequired(false)
+                .HasMaxLength(100);
+
+            // Table Name
+            builder.ToTable("placeholders");
+        }
+    }
+}
