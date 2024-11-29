a24494d8944b1f056461dd42da70cedb1a786f4a
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-29 15:18:13 +0300

    ApplicationRoleClaimConfiguration Created

---
+++ src/AkilliPrompt.Persistance/EntityFramework/Configurations/ApplicationRoleClaimConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AkilliPrompt.Domain.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Persistance.EntityFramework.Configurations
+{
+    public sealed class ApplicationRoleClaimConfiguration : IEntityTypeConfiguration<ApplicationRoleClaim>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationRoleClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetRoleClaims table
+            builder.ToTable("application_role_claims");
+        }
+    }
+}
