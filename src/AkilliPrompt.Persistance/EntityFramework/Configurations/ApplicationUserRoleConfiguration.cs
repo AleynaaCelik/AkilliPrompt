6d0b9e4d37d0c23c8bdbc8bead923e90ea6af413
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-29 16:47:54 +0300

    ApplicationUserRoleConfiguration Created

---
+++ src/AkilliPrompt.Persistance/EntityFramework/Configurations/ApplicationUserRoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AkilliPrompt.Domain.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Persistance.EntityFramework.Configurations
+{
+    public sealed class ApplicationUserRoleConfiguration : IEntityTypeConfiguration<ApplicationUserRole>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUserRole> builder)
+        {
+            // Primary key
+            builder.HasKey(r => new { r.UserId, r.RoleId });
+
+            // Maps to the AspNetUserRoles table
+            builder.ToTable("application_user_roles");
+        }
+    }
+}
