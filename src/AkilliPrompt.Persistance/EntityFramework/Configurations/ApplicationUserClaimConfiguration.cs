d7ef523c9e845f85c2ac2cf6b34c51c20fb8838f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-29 16:43:05 +0300

    ApplicationUserClaimConfiguration Created

---
+++ src/AkilliPrompt.Persistance/EntityFramework/Configurations/ApplicationUserClaimConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AkilliPrompt.Domain.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Persistance.EntityFramework.Configurations
+{
+    public sealed class ApplicationUserClaimConfiguration : IEntityTypeConfiguration<ApplicationUserClaim>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUserClaim> builder)
+        {
+            // Primary key
+            builder.HasKey(rc => rc.Id);
+
+            // Maps to the AspNetUserClaims table
+            builder.ToTable("application_user_claims");
+        }
+    }
+}
