01e66376bcdf402f08ca5d5f3407c28b5f42a9b6
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-29 17:37:32 +0300

    ConvertionExtensions Created

--- src/AkilliPrompt.Domain/Entities/UserLikePrompt.cs
+++ src/AkilliPrompt.Domain/Entities/UserLikePrompt.cs
@@ -11,7 +11,7 @@ namespace AkilliPrompt.Domain.Entities
     public sealed  class UserLikePrompt:EntityBase
     {
         public long UserId { get; set; }
-        public ApplicationUser ApplicationUser { get; set; }
+        public ApplicationUser User { get; set; }
 
         public long PromptId {  get; set; }
         public Prompt Prompt { get; set; }
