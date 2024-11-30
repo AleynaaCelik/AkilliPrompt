aea8b2cf087df43ffc2b93d4e1dbf54c07a00328
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-11-30 23:33:16 +0300

    AzureKeyVaultSettings Created

---
+++ src/AkilliPrompt.WebApi/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using AkilliPrompt.Domain.Identity;
+using AkilliPrompt.Persistence.EntityFramework.Contexts;
+using Microsoft.AspNetCore.Identity;
+using System.Reflection;
+
+namespace AkilliPrompt.WebApi
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebApi(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+
+            return services;
+        }
+    }
+}
