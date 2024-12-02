0e6efb962b0c1ee27e808d1337fee733c292c75c
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-02 10:30:31 +0300

    RefreshToken Created

--- src/AkilliPrompt.Domain/Entities/RefreshToken.cs
+++ src/AkilliPrompt.Domain/Entities/RefreshToken.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AkilliPrompt.Domain.Common;
+using AkilliPrompt.Domain.Identity;
 
-namespace AkilliPrompt.Domain.Entities
+namespace AkilliPrompt.Domain.Entities;
+public class RefreshToken : EntityBase
 {
-    internal class RefreshToken
-    {
-    }
+    public string Token { get; set; }
+    public DateTime Expires { get; set; }
+    public string CreatedByIp { get; set; }
+    public string SecurityStamp { get; set; }
+    public DateTime? Revoked { get; set; }
+    public string? RevokedByIp { get; set; }
+    public Guid UserId { get; set; }
+    public virtual ApplicationUser User { get; set; }
 }
