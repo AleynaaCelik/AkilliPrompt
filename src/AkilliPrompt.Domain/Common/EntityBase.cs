74f412bbf225b4ef1792dcdc2d77fe72a9b3544f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-02 10:23:47 +0300

    IDomainEvent Created

--- src/AkilliPrompt.Domain/Common/EntityBase.cs
+++ src/AkilliPrompt.Domain/Common/EntityBase.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace AkilliPrompt.Domain.Common;
 
-namespace AkilliPrompt.Domain.Common
+public abstract class EntityBase : ICreatedByEntity, IModifiedByEntity
 {
-    public abstract class EntityBase:ICreatedByEntity,IModifiedByEntity
-    {
-        public long Id { get; set; }
-        public string? CreatedByUserId { get; set; }
-        public DateTimeOffset CreatedOn { get; set; }
+    public Guid Id { get; set; }
 
-        public string? ModifiedByUserId { get; set; }
-        public DateTimeOffset? ModifiedAt { get; set; }
-       
-    }
+    public string? CreatedByUserId { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+
+    public string? ModifiedByUserId { get; set; }
+    public DateTimeOffset? ModifiedAt { get; set; }
+
+    private readonly List<IDomainEvent> _domainEvents = [];
+    public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
+    protected void ClearDomainEvents() => _domainEvents.Clear();
 }
