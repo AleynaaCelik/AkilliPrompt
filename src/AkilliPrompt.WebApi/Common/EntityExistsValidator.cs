0daf14b6709f4a3418eac3a39d5f4e21081ae9f7
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-03 23:29:34 +0300

    EntityExistsValidator Created

---
+++ src/AkilliPrompt.WebApi/Common/EntityExistsValidator.cs
@@ -0,0 +1,41 @@
+﻿using AkilliPrompt.Domain.Common;
+using FluentValidation;
+
+namespace AkilliPrompt.WebApi.Common
+{
+    public abstract class EntityExistsValidator<TEntity, TCommand> : AbstractValidator<TCommand>
+     where TEntity : EntityBase
+    {
+        private readonly IExistenceService<TEntity> _existenceService;
+
+        public EntityExistsValidator(IExistenceService<TEntity> existenceService)
+        {
+            _existenceService = existenceService;
+
+            RuleFor(e => GetEntityId(e))
+                .NotEmpty()
+                .WithMessage($"Lütfen geçerli bir {typeof(TEntity).Name} kimliği sağlayın.")
+                .MustAsync(EntityExists)
+                .WithMessage($"Belirtilen {typeof(TEntity).Name} mevcut değil.");
+        }
+
+        /// <summary>
+        /// Extracts the entity ID from the command.
+        /// </summary>
+        /// <param name="command">The command containing the ID.</param>
+        /// <returns>The entity ID.</returns>
+        protected abstract Guid GetEntityId(TCommand command);
+
+        /// <summary>
+        /// Checks if the entity exists using the ExistenceService.
+        /// </summary>
+        /// <param name="id">Entity ID.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>True if exists; otherwise, false.</returns>
+        private async Task<bool> EntityExists(Guid id, CancellationToken cancellationToken)
+        {
+            return await _existenceService.ExistsAsync(id, cancellationToken);
+        }
+
+    }
+}
