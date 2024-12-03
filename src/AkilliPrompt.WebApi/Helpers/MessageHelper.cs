fcd779c8fa7ac02ca01b9cd5758c721aa3dffc2f
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-03 23:36:07 +0300

    MessageHelper Created

---
+++ src/AkilliPrompt.WebApi/Helpers/MessageHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace AkilliPrompt.WebApi.Helpers
+{
+    public static class MessageHelper
+    {
+        public static string GeneralValidationErrorMessage => "Bir veya daha fazla validasyon hatası oluştu.";
+        public static string GeneralErrorMessage => "Beklenmedik bir sunucu hatası oluştu.";
+
+        public static string GetApiSuccessCreatedMessage(string entityName) => $"{entityName} başarıyla oluşturuldu.";
+        public static string GetApiSuccessUpdatedMessage(string entityName) => $"{entityName} başarıyla güncellendi.";
+        public static string GetApiSuccessDeletedMessage(string entityName) => $"{entityName} başarıyla silindi.";
+    }
+}
