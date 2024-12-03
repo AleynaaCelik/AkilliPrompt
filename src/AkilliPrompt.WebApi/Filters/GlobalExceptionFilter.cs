865c766cc76c06bc8c9c710e5e4de4e87c6b368e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-03 23:51:35 +0300

    ResponseDto Created

--- src/AkilliPrompt.WebApi/Filters/GlobalExceptionFilter.cs
+++ src/AkilliPrompt.WebApi/Filters/GlobalExceptionFilter.cs
@@ -1,26 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
 using FluentValidation;
+using AkilliPrompt.WebApi.Helpers;
+using AkilliPrompt.WebApi.Models;
 
 namespace AkilliPrompt.WebApi.Filters
 {
     public class GlobalExceptionFilter : IExceptionFilter
     {
         private readonly ILogger<GlobalExceptionFilter> _logger;
+
         public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
         {
             _logger = logger;
         }
+
         public void OnException(ExceptionContext context)
         {
             _logger.LogError(context.Exception, context.Exception.Message);
 
             context.ExceptionHandled = true;
 
-            // Eğer hata bir doğrulama hatası ise
             if (context.Exception is ValidationException validationException)
             {
-
                 var responseMessage = MessageHelper.GeneralValidationErrorMessage;
 
                 var errors = validationException.Errors
@@ -28,7 +30,6 @@ namespace AkilliPrompt.WebApi.Filters
                     .Select(g => new ValidationError(g.Key, g.Select(e => e.ErrorMessage)))
                     .ToList();
 
-                // 400 - Bad Request
                 context.Result = new BadRequestObjectResult(ResponseDto<object>.Error(responseMessage, errors))
                 {
                     StatusCode = StatusCodes.Status400BadRequest
@@ -36,7 +37,6 @@ namespace AkilliPrompt.WebApi.Filters
             }
             else
             {
-                // Diğer tüm hatalar için 500 - Internal Server Error
                 context.Result = new ObjectResult(ResponseDto<object>.Error(MessageHelper.GeneralErrorMessage))
                 {
                     StatusCode = StatusCodes.Status500InternalServerError
