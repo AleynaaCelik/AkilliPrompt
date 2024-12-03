c8b4b1502816cadfb92e579451fd229b5542cbd1
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-03 23:35:32 +0300

    CacheKeysHelper Created

---
+++ src/AkilliPrompt.WebApi/Helpers/CacheKeysHelper.cs
@@ -0,0 +1,8 @@
+﻿namespace AkilliPrompt.WebApi.Helpers
+{
+    public static class CacheKeysHelper
+    {
+        public static string GetAllCategoriesKey => "GetAllCategories";
+        public static string GetByIdCategoryKey(Guid id) => $"GetByIdCategory:{id}";
+    }
+}
