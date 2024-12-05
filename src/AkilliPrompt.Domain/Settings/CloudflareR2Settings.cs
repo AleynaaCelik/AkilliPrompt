94138130f50bfee5dbd1e1c12300d87584fa4aca
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-05 23:52:49 +0300

    [Feature]: Cloudflare R2integration add.

---
+++ src/AkilliPrompt.Domain/Settings/CloudflareR2Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Settings
+{
+    public sealed record CloudflareR2Settings
+    {
+        public string ServiceUrl { get; set; }
+        public string AccessKey { get; set; }
+        public string SecretKey { get; set; }
+        public string PromptPicsBucketName { get; set; }
+        public string UserPicsBucketName { get; set; }
+    }
+}
