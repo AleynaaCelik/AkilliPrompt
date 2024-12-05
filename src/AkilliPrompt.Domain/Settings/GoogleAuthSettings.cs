6e30b6fb1e550aafb843ed84e72422f590d4cb27
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-05 23:52:09 +0300

    [Feature]: Google Login Auth operation completed.

---
+++ src/AkilliPrompt.Domain/Settings/GoogleAuthSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkilliPrompt.Domain.Settings
+{
+    public sealed record GoogleAuthSettings
+    {
+        public string ClientId { get; init; }
+
+        public GoogleAuthSettings(string clientId)
+        {
+            ClientId = clientId;
+        }
+
+        public GoogleAuthSettings()
+        {
+
+        }
+    };
+}
