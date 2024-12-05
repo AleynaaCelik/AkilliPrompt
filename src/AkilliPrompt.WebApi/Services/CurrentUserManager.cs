233b31bc5ae3b55fa5a5c35ee38673bb5fe68dd2
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-05 23:31:15 +0300

    CurrentUserManager Created

---
+++ src/AkilliPrompt.WebApi/Services/CurrentUserManager.cs
@@ -0,0 +1,40 @@
+﻿using AkilliPrompt.Domain.Helpers;
+using AkilliPrompt.Persistance.Services;
+
+namespace AkilliPrompt.WebApi.Services
+{
+    public sealed class CurrentUserManager : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IWebHostEnvironment _env;
+
+        public CurrentUserManager(IHttpContextAccessor httpContextAccessor, IWebHostEnvironment env)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _env = env;
+        }
+
+        // public long? UserId => GetUserId();
+        public Guid UserId => GetUserId();
+
+        public string IpAddress => GetIpAddress();
+
+        private Guid GetUserId()
+        {
+            var userId = _httpContextAccessor.HttpContext?.User.FindFirst("uid")?.Value;
+
+            return userId is null ? Guid.Empty : Guid.Parse(userId);
+        }
+
+        private string GetIpAddress()
+        {
+            if (_env.IsDevelopment())
+                return IpHelper.GetIpAddress();
+
+            if (_httpContextAccessor.HttpContext.Request.Headers.ContainsKey("X-Forwarded-For"))
+                return _httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"];
+            else
+                return _httpContextAccessor.HttpContext.Connection.RemoteIpAddress?.ToString();
+        }
+    }
+}
